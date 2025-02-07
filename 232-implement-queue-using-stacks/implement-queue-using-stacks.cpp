339ecf6b17591948806fd4a92433b4b84371a2e8
Mridul Mehra <113982798+mridulmehra@users.noreply.github.com>
2025-02-07 17:07:45 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (26.45%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -0,0 +1,34 @@
+class MyQueue {
+
+private:
+    queue<int> input;
+    queue<int> output;
+
+public:
+    MyQueue() {}
+
+    void push(int x) {
+        input.push(x);
+    }
+
+    int pop() {
+        peek();
+        int val = output.front();
+        output.pop();
+        return val;
+    }
+
+    int peek() {
+        if (output.empty()) {
+            while (!input.empty()) {
+                output.push(input.front());
+                input.pop();
+            }
+        }
+        return output.front();
+    }
+
+    bool empty() {
+        return input.empty() && output.empty();
+    }
+};
