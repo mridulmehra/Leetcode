a0e11b01da8147b1dd248298970540a6cb5fa477
Mridul Mehra <113982798+mridulmehra@users.noreply.github.com>
2025-02-23 15:33:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (50.12%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    unordered_map<string, bool> memo;
-    
-    bool result(string &s, int cnt, int ind, int n) {
-        if (ind == n) return cnt == 0;
-        if (cnt < 0) return false;
-
-        string key = to_string(cnt) + "_" + to_string(ind);
-        if (memo.find(key) != memo.end()) return memo[key];
-
-        if (s[ind] == '(') cnt++;
-        else if (s[ind] == ')') cnt--;
-        else {
-            return memo[key] = (result(s, cnt + 1, ind + 1, n) || 
-                                result(s, cnt - 1, ind + 1, n) || 
-                                result(s, cnt, ind + 1, n));
-        }
-
-        return memo[key] = result(s, cnt, ind + 1, n);
-    }
-    
     bool checkValidString(string s) {
-        return result(s, 0, 0, s.size());  
+        int min = 0, max = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                min += 1;
+                max += 1;
+            } else if (s[i] == ')') {
+                min -= 1;
+                max -= 1;
+            } else {
+                min -= 1;
+                max += 1;
+            }
+            if (min < 0)
+                min = 0;
+            if (max < 0)
+                return false;
+        }
+        return (min == 0);
     }
 };
